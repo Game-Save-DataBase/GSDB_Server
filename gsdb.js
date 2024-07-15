5527ca58c7d37f11e2bbdb9d8785f87da4a2f40c
jboppdev <jorgbell@ucm.es>
2024-07-15 13:22:41 +0200

    api de ejemplo (libros)

--- gsdb.js
+++ gsdb.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const routes = require("./routes/api/books");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+// use the cors middleware with the
+// origin and credentials options
+app.use(cors({ origin: true, credentials: true }));
+
+// use the body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// use the routes module as a middleware
+// for the /api/books path
+//esto es un include para que las aplicaciones que usen nuestra BBDD puedan acceder a ella por bloqueos de seguridad
+app.use("/api/books", routes);
 
 // Connect Database
 connectDB();
