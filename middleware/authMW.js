ada3b1a1be61c7427779985a540f94925ea07c8c
jorgbell <jorgbell@ucm.es>
2025-08-13 18:04:52 +0100

    print errores req.isauth

--- middleware/authMW.js
+++ middleware/authMW.js
@@ -2,18 +2,33 @@ const httpResponses = require('../utils/httpResponses');
 
 // Middleware para autenticar obligatoriamente
 function authenticateMW(req, res, next) {
-  if (req.isAuthenticated() || process.env.DEV_MODE === 'true') {
-    return next();
+  try {
+    console.log("REQ.ISAUTH de la funcion authenticateMW:")
+    console.log(req.isAuthenticated())
+    if (req.isAuthenticated() || process.env.DEV_MODE === 'true') {
+      return next();
+    }
+    return httpResponses.unauthorized(res, 'Not authorized, you must be logged in.');
+
+  } catch (err) {
+    console.error(err)
   }
-  return httpResponses.unauthorized(res, 'Not authorized, you must be logged in.');
 }
 
 // Middleware para comprobar si hay usuario logueado (sin error)
 function checkLoggedUserMW(req, res, next) {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    req.loggedUser = req.user; // Guarda el usuario en req.loggedUser
-  } else {
-    req.loggedUser = null;
+  try {
+    console.log("REQ.ISAUTH de la funcion checkloggedusermw:")
+    console.log(req.isAuthenticated())
+
+    if (req.isAuthenticated && req.isAuthenticated()) {
+      req.loggedUser = req.user; // Guarda el usuario en req.loggedUser
+    } else {
+      req.loggedUser = null;
+    }
+
+  } catch (err) {
+    console.error(err)
   }
   next();
 }
